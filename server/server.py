375203bff1bca3e5f3412effb38bb2fd639bf167
Siksss <matias.sotti@outlook.fr>
2025-05-15 07:41:39 +0200

    Liaison flask nextjs

---
+++ server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+#app instance
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/api/home', methods=['GET'])
+def return_home():
+    return jsonify({
+        "message": "yo la team",
+        "people" : ['John', 'Jane', 'Doe']
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
